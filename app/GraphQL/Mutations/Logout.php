aad245f273e7742aac38c6df94e58a1cf7131206
Jc <jcsernicula9@gmail.com>
2025-04-19 21:01:01 +0800

    implement backend auth

---
+++ app/GraphQL/Mutations/Logout.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+final readonly class Logout
+{
+    /** @param  array{}  $args */
+    public function __invoke(null $_, array $args): ?User
+    {
+        $guard = Auth::guard('web');
+        $user = $guard->user();
+
+        $guard->logout();
+        return $user;
+    }
+}
--- app/GraphQL/Mutations/Register.php
+++ app/GraphQL/Mutations/Register.php
@@ -5,22 +5,22 @@ declare(strict_types=1);
 namespace App\GraphQL\Mutations;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 final readonly class Register
 {
     /** @param  array{}  $args */
-    public function __invoke(null $_, array $args)
+    public function __invoke(null $_, array $args): User
     {
-        // $validated = validator($args, [
-        //     'email' => 'required|email|unique:users,email',
-        //     'name' => 'required|string|max:255',
-        //     'password' => 'required|min:6',
-        // ])->validate();
-
         $user = User::create([
             'name' => $args['name'],
             'email' => $args['email'],
             'password' => $args['password'],
         ]);
+
+        $guard = Auth::guard('web');
+        $guard->login($user);
+
+        return $user;
     }
 }
