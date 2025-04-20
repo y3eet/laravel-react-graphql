a91db167022897ee19546baadee0b9e0b790445b
Jc <jcsernicula9@gmail.com>
2025-04-20 16:49:12 +0800

    change image directory

--- src/components/routes/auth-page.tsx
+++ src/components/routes/auth-page.tsx
@@ -71,7 +71,7 @@ export default function AuthPage() {
       </div>
       <div className="relative hidden bg-muted lg:block">
         <img
-          src="/src/assets/tokyo-pink.png"
+          src="/tokyo-pink.png"
           alt="Image"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
